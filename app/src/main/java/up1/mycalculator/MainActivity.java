baca8694e6a30f51674432dd1ca94653f7c1dcc7
somkiat <somkiat.p@gmail.com>
2016-01-25 23:00:21 +0700

    Add id and handling event of calculate button was clicked

--- app/src/main/java/up1/mycalculator/MainActivity.java
+++ app/src/main/java/up1/mycalculator/MainActivity.java
@@ -2,6 +2,10 @@ package up1.mycalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText firstNumber = (EditText) findViewById(R.id.firstNumber);
+        final EditText secondNumber = (EditText) findViewById(R.id.secondNumber);
+        final TextView resultTextView = (TextView)findViewById(R.id.result);
+
+        Button calculateButton = (Button) findViewById(R.id.calculateButton);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int first = Integer.parseInt(firstNumber.getText().toString());
+                int second = Integer.parseInt(secondNumber.getText().toString());
+                int result = first + second;
+                resultTextView.setText(String.valueOf(result));
+            }
+        });
+
     }
 }
